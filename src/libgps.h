d13076a7a675beaca7b1ef9eb0be825466fd35e3
Lars Hoffmann <l.hoffmann@fz-juelich.de>
2025-07-15 07:13:25 +0200

    Update Doxygen documentation.

--- src/libgps.h
+++ src/libgps.h
@@ -1,3 +1,82 @@
+/*
+  This file is part of the GPS Code Collection.
+  
+  the GPS Code Collections is free software: you can redistribute it
+  and/or modify it under the terms of the GNU General Public License
+  as published by the Free Software Foundation, either version 3 of
+  the License, or (at your option) any later version.
+  
+  The GPS Code Collection is distributed in the hope that it will be
+  useful, but WITHOUT ANY WARRANTY; without even the implied warranty
+  of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+  General Public License for more details.
+  
+  You should have received a copy of the GNU General Public License
+  along with the GPS Code Collection. If not, see
+  <http://www.gnu.org/licenses/>.
+  
+  Copyright (C) 2019-2025 Forschungszentrum Juelich GmbH
+*/
+
+/*! 
+  \file
+  GPS Code Collection library declarations.
+*/
+
+/*! 
+  \mainpage
+
+  The GPS Code Collection enables data processing and analysis
+  for of GPS/RO observations.
+
+  \section Introduction
+
+  The source code of the GPS Code Collection is available from the
+  [git repository](https://github.com/slcs-jsc/airs). Please see the
+  [README.md](https://github.com/slcs-jsc/airs/blob/master/README.md)
+  in the git repository for introductory information. More information
+  can be found in the [user manual](https://slcs-jsc.github.io/airs).
+  
+  This doxygen manual contains information about the algorithms and
+  data structures used in the code. Please refer to the `libairs.h'
+  documentation for a first overview.
+  
+  \section References
+  
+  For citing the model in scientific publications, please see
+  [CITATION.cff](https://github.com/slcs-jsc/airs/blob/master/CITATION.cff).
+  
+  \section License
+  
+  The GPS Code Collection is being develop at the Jülich Supercomputing Centre,
+  Forschungszentrum Jülich, Germany.
+  
+  the GPS Code Collection is distributed under the terms of the
+  [GNU General Public License v3.0](https://github.com/slcs-jsc/airs/blob/master/COPYING).
+  
+  \section Contributing
+  
+  We are interested in supporting operational and research
+  applications with the GPS Code Collection.
+  
+  You can submit bug reports or feature requests on the
+  [issue tracker](https://github.com/slcs-jsc/airs/issues).
+  
+  Proposed code changes and fixes can be submitted as
+  [pull requests](https://github.com/slcs-jsc/airs/pulls).
+  
+  Please do not hesitate to contact us if you have any questions or
+  need assistance.
+  
+  \section Contact
+  
+  Dr. Lars Hoffmann
+  
+  Jülich Supercomputing Centre, Forschungszentrum Jülich
+  
+  e-mail: <l.hoffmann@fz-juelich.de>
+*/
+
 #include <netcdf.h>
 #include <gsl/gsl_multifit.h>
 #include <gsl/gsl_poly.h>
