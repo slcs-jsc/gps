d38544a6ddf29cc9413d07964ae2dc4aa1bfc526
lars2015 <lars2015@users.noreply.github.com>
2026-03-14 07:06:51 +0000

    deploy: cdaebf58e2044732b913f5bb16f63d3ccd8ae8d9

---
+++ doxygen/search/files_3.js
@@ -0,0 +1,5 @@
+var searchData=
+[
+  ['libgps_2ec_0',['libgps.c',['../libgps_8c.html',1,'']]],
+  ['libgps_2eh_1',['libgps.h',['../libgps_8h.html',1,'']]]
+];
---
+++ doxygen/search/variables_5.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['z_0',['z',['../structgps__t.html#ad1578ddb7b4cb5d5f4d5dea1ce1a4ca5',1,'gps_t']]]
+];
